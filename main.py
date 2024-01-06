82de916f32d55f206c2100dcdb1005a12e60a30b
kanikevi <01194270@pw.edu.pl>
2024-01-06 23:24:29 +0100

    added selection of map creation variability

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import numpy as np
+from Map import Map
+from Path import Path
 import matplotlib.pyplot as plt
-from classes import Map, Path
 
-n = int(input("Enter the size of the field: "))
+choice = str(input('Which map creation option suits you:\nprint m for manual, a for automatic or f for file creation.\n'))
+path = None
+
+map_size = int(input("Enter the size of the field: "))
 draught = float(input("Enter the draught in metres: "))
-map = Map(n, draught)
 
-map.plot_map()
+
+map = Map(map_size, draught, choice)
+map.create_map_from_choice(choice)
+
+map.plot_map_with_title()
 plt.show()
 
-path = Path(map, n)
-path.find_shortest_path()
+path = Path(map, map_size)
+path.find_paths()
 path.visualize_path()
