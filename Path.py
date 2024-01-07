f6b4c528b658027c590f2bab171ca70a9dd27b46
kanikevi <01194270@pw.edu.pl>
2024-01-07 02:17:37 +0100

    bugs edited, added a check for matching paths

--- Path.py
+++ Path.py
@@ -5,12 +5,13 @@ from PathNotFound import PathNotFound
 # Define a class for finding and visualizing the shortest path on the map
 class Path:
     def __init__(self, map_obj, map_size):
-        self.map_obj = map_obj
-        self.map_size = map_size
         if map_size <= 0:
             raise ValueError('N should be a positive number')
-        self.first_path_of_matrix = None
-        self.second_path_of_matrix = None
+                
+        self.map_obj = map_obj
+        self.map_size = map_size
+        self.first_path = None
+        self.second_path = None
     
     # Method to find the shortest path using BFS
     def find_path(self, start, end):
@@ -34,16 +35,24 @@ class Path:
 
     def find_paths(self):
         start1, end1 = (0, 0), (self.map_size - 1, self.map_size - 1)
-        self.first_path_of_matrix = self.find_path(start1, end1)
+        self.first_path = self.find_path(start1, end1)
 
         start2, end2 = (self.map_size - 1, self.map_size - 1), (0, 0)
-        self.second_path_of_matrix = self.find_path(start2, end2)
+        self.second_path = self.find_path(start2, end2)
+        
+        if self.first_path is None:
+            raise PathNotFound()
+        else:
+            # Check if paths are the same, if yes, keep only one
+            if set(self.first_path) == set(self.second_path):
+                self.path_matrix = self.first_path
+            else:
+                self.path_matrix = (self.first_path, self.second_path)
 
 
     # Reconstruct the path from the end to the start using the visited dictionary
     def reconstruct_path(self, visited, end):
         if end not in visited:
-            print("There is no valid path")
             return None       
 
         path = []
@@ -54,17 +63,23 @@ class Path:
 
         return path
 
-    def visualize_path(self):
-        if self.first_path_of_matrix is None:
-            return
+    def visualize_path(self, path_color='white', alt_path_color='#F0E68C', marker_size=8):
+        
+        if isinstance(self.path_matrix, tuple):
+            self.first_path, self.second_path = self.path_matrix
 
-        x1_coords, y1_coords = zip(*self.first_path_of_matrix)
-        x2_coords, y2_coords = zip(*self.second_path_of_matrix)
+            x1_coords, y1_coords = zip(*self.first_path)
+            x2_coords, y2_coords = zip(*self.second_path)
 
+            plt.plot(y1_coords, x1_coords, marker='o', markersize=marker_size, color=path_color, label='Path1')
+            plt.plot(y2_coords, x2_coords, marker='x', markersize=marker_size, color=alt_path_color, label='Path2')
+
+        else:
+            x_coords, y_coords = zip(*self.path_matrix)
+            plt.plot(y_coords, x_coords, marker='o', markersize=marker_size, color=path_color, label='Path')
+            
         self.map_obj.plot_map()
-        plt.title(r'Close the window to excape')
-
-        plt.plot(y1_coords, x1_coords, marker='o', markersize=8, color='white', label='Path1')
-        plt.plot(y2_coords, x2_coords, marker='x', markersize=8, color='#F0E68C', label='Path2')
+        plt.title('Close the window to escape')
         plt.legend()
         plt.show()
+        
