a5eeac47bbef6ffe8ceacb80ef91f2ee2564516d
kanikevi <01194270@pw.edu.pl>
2024-01-07 20:12:05 +0100

    file reformatted

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ from Map import Map
 from Path import Path
 import matplotlib.pyplot as plt
 
-choice = str(input('Which map creation option suits you:\nprint m for manual, a for automatic, or f for file creation.\n')).lower()
+choice = str(
+    input(
+        "Which map creation option suits you:\nprint m for manual, a for automatic, or f for file creation.\n"
+    )
+).lower()
 
-if choice == 'f':
-    print("\033[92mIf you are using your own file, take these facts into account:\n"
-      " - The first line must contain values for map_size and draught, separated by spaces.\n"
-      " - The following lines must contain depths for each cell.\033[0m")
-    
-    file_path = str(input('Enter the file path:'))
+if choice == "f":
+    print(
+        "\033[92mIf you are using your own file, take these facts into account:\n"
+        " - The first line must contain values for map_size and draught, separated by spaces.\n"
+        " - The following lines must contain depths for each cell.\033[0m"
+    )
+
+    file_path = str(input("Enter the file path:"))
     first_line = np.loadtxt(file_path, max_rows=1)
-    first_line_str = ' '.join(map(str, first_line))
-    map_size, draught = map(int, map(float, first_line_str.split(' ')))
+    first_line_str = " ".join(map(str, first_line))
+    map_size, draught = map(int, map(float, first_line_str.split(" ")))
 else:
     file_path = None
     map_size = int(input("Enter the size of the field: "))
@@ -23,7 +29,8 @@ else:
 map_instance = Map(map_size, draught, choice, file_path)
 map_instance.create_map_from_choice(choice)
 
-map_instance.plot_map_with_title()
+map_instance.plot_map()
+plt.title("To see the path, close this window")
 plt.show()
 
 path_instance = Path(map_instance, map_size)
