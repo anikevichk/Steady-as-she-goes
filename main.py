0fc5ef64a770b91b5f47ded6a79001859c5077bd
kanikevi <01194270@pw.edu.pl>
2024-01-07 02:20:20 +0100

    аdded code to invoke the function for reading the map from a file

--- main.py
+++ main.py
@@ -3,19 +3,29 @@ from Map import Map
 from Path import Path
 import matplotlib.pyplot as plt
 
-choice = str(input('Which map creation option suits you:\nprint m for manual, a for automatic or f for file creation.\n'))
-path = None
+choice = str(input('Which map creation option suits you:\nprint m for manual, a for automatic, or f for file creation.\n')).lower()
 
-map_size = int(input("Enter the size of the field: "))
-draught = float(input("Enter the draught in metres: "))
+if choice == 'f':
+    print("\033[92mIf you are using your own file, take these facts into account:\n"
+      " - The first line must contain values for map_size and draught, separated by spaces.\n"
+      " - The following lines must contain depths for each cell.\033[0m")
+    
+    file_path = str(input('Enter the file path:'))
+    first_line = np.loadtxt(file_path, max_rows=1)
+    first_line_str = ' '.join(map(str, first_line))
+    map_size, draught = map(int, map(float, first_line_str.split(' ')))
+else:
+    file_path = None
+    map_size = int(input("Enter the size of the field: "))
+    draught = float(input("Enter the draught in metres: "))
 
 
-map = Map(map_size, draught, choice)
-map.create_map_from_choice(choice)
+map_instance = Map(map_size, draught, choice, file_path)
+map_instance.create_map_from_choice(choice)
 
-map.plot_map_with_title()
+map_instance.plot_map_with_title()
 plt.show()
 
-path = Path(map, map_size)
-path.find_paths()
-path.visualize_path()
+path_instance = Path(map_instance, map_size)
+path_instance.find_paths()
+path_instance.visualize_path()
